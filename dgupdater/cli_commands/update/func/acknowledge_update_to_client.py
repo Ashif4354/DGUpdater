7e283496c3b2fa857986f0d247a1520650846f17
Ashif4354 <ashif.abhas@gmail.com>
2025-06-24 16:13:23 +0530

    Refactor update acknowledgment to use console output instead of messagebox and remove unused main check in ask_to_be_updated

--- dgupdater/cli_commands/update/func/acknowledge_update_to_client.py
+++ dgupdater/cli_commands/update/func/acknowledge_update_to_client.py
@@ -1,4 +1,14 @@
-from tkinter import messagebox  
+from os import system
 
 def acknowledge_update_to_client() -> None:
-    messagebox.showinfo("Success", "Updated to the latest version. Please restart the application.")
+    system('cls')
+
+    print("✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅")
+    print("✅✅  🙌 Updated to the latest version. Restart the application to see the changes. 🙌  ✅✅")
+    print('✅✅  🙌 In Tamil: "Update Mudinjiduchu da. App-a restart pannu". 🙌                    ✅✅')
+    print("✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅")
+
+    input("\nPress Enter to continue...")  # Wait for user input before closing the console
+
+
+acknowledge_update_to_client()
